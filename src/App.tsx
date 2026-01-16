e44a0ffcbde937ae303cc7053d2de8e73acd7280
GusenovMarat <gusenovmaratr@gmail.com>
2026-01-16 15:54:34 +0300

    add header component to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 const App = () => {
   return (
-    <h1>Hello</h1>
+    <header className="header">
+			<div className="header_top"></div>
+			<div className="header_bottom">
+				<div className ="header_logo">
+					<div className="header_logo-icon"></div>
+				</div>
+				<div className ="header_main">
+					<ul>
+						<li className="header_item">home</li>
+						<li className="header_item">news</li>
+						<li className="header_item">podcasts</li>
+						<li className="header_item">resourses</li>
+					</ul>
+				</div>
+			</div>
+			<div className ="header_right"></div>
+		</header>
   )
 }
 

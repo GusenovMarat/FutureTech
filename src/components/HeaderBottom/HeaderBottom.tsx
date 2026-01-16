730f96829dcd1ed3a703f6aba432c5e0b0660d1c
GusenovMarat <gusenovmaratr@gmail.com>
2026-01-16 17:43:47 +0300

    decomposing header (headerBottom)

---
+++ src/components/HeaderBottom/HeaderBottom.tsx
@@ -0,0 +1,15 @@
+import HeaderList from "../HeaderList/HeaderList";
+
+const HeaderBottom = () => {
+  return (
+		<div className="header-bottom">
+			<div className ="header-bottom_logo">
+				<div className="header-bottom_icon"></div>
+			</div>
+			<HeaderList />
+			<div className ="header-right"></div>
+		</div>
+  )
+}
+
+export default HeaderBottom;
---
+++ src/components/HeaderTop/HeaderTop.tsx
@@ -0,0 +1,17 @@
+
+const HeaderTop = () => {
+  return (
+    <header className="header">
+			<div className="header-top"></div>
+			<div className="header-bottom">
+				<div className ="header-logo">
+					<div className="header-logo_icon"></div>
+				</div>
+				<HeaderList />
+				<div className ="header-right"></div>
+			</div>
+		</header>
+  )
+}
+
+export default App;
